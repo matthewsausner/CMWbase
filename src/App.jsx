cb1ed68cd66ee5ff4d2644d2d126c91899c5678e
Saus <matthew.sausner@gmail.com>
2026-01-12 19:08:55 -0500

    home page css, no working links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import spliffsStill from './assets/spliffs_still.jpg'
+import header from './assets/name.png'
+import store from './assets/store.png'
+import links from './assets/links.png'
+import tour from './assets/tour.png'
 import './App.css'
 
 function App() {
@@ -8,11 +11,26 @@ function App() {
 
   return (
     <>
-      <h1>Claire Something</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className="site_header">
+          <img src={header} alt="header" />
+      </header>
+      <div className="container_row">
+        <div className="image_container">
+          <img src={spliffsStill} alt="homePage" />
+        </div>
+        <div className="links_container">
+          <div className="links_grid">
+            <div className="store_link">
+              <input type="image" src={store} alt="store button"/>
+            </div>
+            <div className="links_link">
+              <input type="image" src={links} alt="links button"/>
+            </div>
+            <div className="tour_link">
+              <input type="image" src={tour} alt="tour button"/>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
