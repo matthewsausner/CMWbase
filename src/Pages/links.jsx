055837096eb5861a1501a17c358c32b7a10c0510
Saus <matthew.sausner@gmail.com>
2026-01-26 18:26:41 -0500

    links page

--- src/Pages/links.jsx
+++ src/Pages/links.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Header from '../Components/Header.jsx'
 import './links.css'
+import bc from '../assets/bc.png'
+import ig from '../assets/ig.png'
+import tidal from '../assets/tidal.png'
+import spotify from '../assets/spotify.png'
 
 const Links = () => {
 	return(
 		<>
 			<Header />
-			<h2> links </h2>
+			<div class="image-grid">
+				<a href="https://www.instagram.com/clrsmthn/">
+					<img className="link ig" src={ig} alt="ig" />
+				</a>
+				<a href="https://clairesomething.bandcamp.com/">
+					<img className="link bc" src={bc} alt="bandcamp" />
+				</a>
+				<br/>
+				<img className="link tidal" src={tidal} alt="tidal" />
+				<a href="https://open.spotify.com/artist/5jTmDxDsCR5NBzi2YEVIB6?si=Eld-CPvTRVuYjG_Z_gx-Ng">
+					<img className="link spotify" src={spotify} alt="spotify" />
+				</a>
+			</div>
 		</>
 	)
 }
