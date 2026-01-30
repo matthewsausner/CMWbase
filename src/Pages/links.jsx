29d6d740f6bf53d667ff295bc25abbb78862ea19
Saus <matthew.sausner@gmail.com>
2026-01-30 10:54:46 -0500

    yt and apple music link

--- src/Pages/links.jsx
+++ src/Pages/links.jsx
@@ -5,6 +5,8 @@ import bc from '../assets/bc.png'
 import ig from '../assets/ig.png'
 import tidal from '../assets/tidal.png'
 import spotify from '../assets/spotify.png'
+import yt from '../assets/yt.png'
+import apple from '../assets/apple.png'
 
 const Links = () => {
 	return(
@@ -22,6 +24,11 @@ const Links = () => {
 				<a href="https://open.spotify.com/artist/5jTmDxDsCR5NBzi2YEVIB6?si=Eld-CPvTRVuYjG_Z_gx-Ng">
 					<img className="link spotify" src={spotify} alt="spotify" />
 				</a>
+				<br/>
+				<a href="https://www.youtube.com/@clrsmthn">
+					<img className="link yt" src={yt} alt="yt" />
+				</a>
+				<img className="link apple" src={apple} alt="apple" />
 			</div>
 		</>
 	)
